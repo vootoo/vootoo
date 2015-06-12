5eff9cd2a565ed15f6d0032c406e86f6d1d8f4b8
chenlb <chenlb2008@gmail.com>
2015-06-12 15:48:20 +0800

    优化类导入

--- vootoo-client/src/main/java/org/vootoo/client/netty/SolrClientChannelInitializer.java
+++ vootoo-client/src/main/java/org/vootoo/client/netty/SolrClientChannelInitializer.java
@@ -20,13 +20,10 @@ package org.vootoo.client.netty;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import org.vootoo.client.netty.protocol.SolrProtocol;
 
 /**
